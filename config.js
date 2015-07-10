c2466cda9aa2cb6cc1983dc6fd25886b0fde8d54
Edznux <edznux@gmail.com>
2015-07-10 21:58:46 +0200

    env based credential

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-module.exports.botToken = "Your-bot-token-here";
+module.exports.botToken = process.env.BOT_KEY;
 
 module.exports.db = {
-	host :"host",
-	user : "user",
-	password : "password",
-	database : "db"
+	host :process.env.DB_HOST,
+	user : process.env.DB_USER,
+	password : process.env.DB_PASS,
+	database : process.env.DB_DB
 };
