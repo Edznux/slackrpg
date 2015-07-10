e53cc00a56f78e8fcabd5fb84b7cdafed70c1062
Edznux <edznux@gmail.com>
2015-07-10 22:27:22 +0200

    ignore config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports.botToken = process.env.BOT_KEY;
-
-module.exports.db = {
-	host :process.env.DB_HOST,
-	user : process.env.DB_USER,
-	password : process.env.DB_PASS,
-	database : process.env.DB_DB
-};
